9e5d0dc8bc50e709da41a4fc52f9e24abeedb83a
Israel Marquez S <s.marquez.israel@gmail.com>
2019-02-05 09:57:09 +0100

    Created script to merge features pickles for an exp device.

---
+++ controlled_exp_analysis/merge_features_pickles.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+import datetime
+import argparse
+
+
+def save_pickle(features_df, out_dir, deployment):
+
+    print "Saving " + deployment + " features pickle."
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    out_file = deployment + "_features.pickle"
+
+    features_df.to_pickle(out_dir + "/" + out_file)
+
+    return 0
+
+def merge_pickles(path):
+
+    features_df = pd.DataFrame()
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            tmp_df = pd.read_pickle(root + "/" + file)
+            features_df = tmp_df.append(tmp_df)
+
+    return features_df
+
+def main():
+
+    print "Script Start."
+    print datetime.datetime.now()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--dir', type=str, required=True, help="Directory where pickles are stored")
+    parser.add_argument('-o', '--outdir', type=str, required=True, help="Directory where comnbined pickles will be saved")
+
+    args = vars(parser.parse_args())
+
+    dir = args["dir"]
+    out_dir = args["outdir"]
+
+    deployment = dir.split("/")[-1]
+
+    features_df = merge_pickles(dir)
+    save_pickle(features_df, out_dir, deployment)
+
+
+    print "Script End."
+    print datetime.datetime.now()
+
+    return 0
+
+if __name__ == '__main__':
+    main()
