fca578db54e32e82c036a1516d8ebb11f6ea52e9
Israel Marquez S <s.marquez.israel@gmail.com>
2019-02-06 13:54:43 +0100

    Fixed bug associated to compute the resolution classes.

--- controlled_exp_analysis/compare_GT_vs_Inference.py
+++ controlled_exp_analysis/compare_GT_vs_Inference.py
@@ -2,7 +2,45 @@ import pandas as pd
 import os
 import datetime
 import argparse
+import numpy as np
+import itertools as iter
+import matplotlib.pyplot as plt
 
+
+
+def plot_differences_CDF(differences, target):
+
+    print "Plotting difference between GT and Inferred CDF."
+
+    fig_path = "./Figures/Differences/"
+
+    if not os.path.exists(fig_path):
+        os.makedirs(fig_path)
+
+    sorted_data = np.sort(differences)
+    yvals = np.arange(len(sorted_data)) / float(len(sorted_data) - 1)
+
+
+    if target == 's':
+        fig_file = "startup_differences.png"
+        plt.grid()
+        plt.title("Startup GT vs Inferred Differences CDF")
+        plt.xlabel("Time [sec]")
+        plt.ylabel("P(x)")
+        plt.plot(sorted_data, yvals)
+        plt.savefig(fig_path + fig_file, dpi=600)
+
+    else:
+        fig_file = "resolution_differences.png"
+        plt.grid()
+        plt.title("Startup GT vs Inferred Differences CDF")
+        plt.xlabel("Class")
+        plt.ylabel("P(x)")
+        plt.plot(sorted_data, yvals)
+        plt.savefig(fig_path + fig_file, dpi=600)
+
+
+    return 0
 
 def get_startup_differences(gt_values, inferred_values):
 
@@ -13,34 +51,50 @@ def get_startup_differences(gt_values, inferred_values):
     for i in range(len(gt_values)):
         startup_differences.append(inferred_values[i] - gt_values)
 
+    plot_differences_CDF(startup_differences, "s")
+
     return startup_differences
 
 def get_resolution_differences(gt_values, inferred_values):
 
     print "Computing difference for resolution."
 
-    res_values = gt_values.unique()
+    resolution_differences_df = pd.DataFrame(columns=["Inferred", "GT", "Difference"])
+    res_values = []
+
+    res_values.extend(gt_values.unique())
+    res_values.extend(inferred_values.unique())
+
+    res_values = np.array(res_values)
+    res_values = np.sort(np.unique(res_values))
 
     res_differences = []
 
-    cont = 0
-    for prediction in inferred_values:
-        res_differences.append(res_values.index(prediction) - res_values.index(inferred_values[cont]))
-        cont += 1
+    for gt, predicted in iter.izip(gt_values, inferred_values):
+        index_gt, = np.where(res_values == gt)
+        index_predicted, = np.where(res_values == predicted)
 
-    return res_differences
+        res_differences.append(index_predicted[0] - index_gt[0])
+
+    resolution_differences_df["Inferred"] = inferred_values
+    resolution_differences_df["GT"] = gt_values
+    resolution_differences_df["Difference"] = res_differences
+
+    plot_differences_CDF(res_differences, "r")
+
+    return resolution_differences_df, res_values
 
 def get_values_to_compare(gt_inference_df, target):
 
     print "Obtaining values to compare."
 
     if target == "s":
-        gt_values = gt_inference_df["startup_mc"]
-        inferred_values = gt_inference_df["startup_time"]
+        gt_values = gt_inference_df["startup_time"]
+        inferred_values = gt_inference_df["startup_mc"]
         get_startup_differences(gt_values, inferred_values)
     else:
-        gt_values = gt_inference_df["resolution_mc"]
-        inferred_values = gt_inference_df["resolution"]
+        gt_values = gt_inference_df["resolution"].astype(int)
+        inferred_values = gt_inference_df["resolution_mc"].astype(int)
         get_resolution_differences(gt_values, inferred_values)
 
 
